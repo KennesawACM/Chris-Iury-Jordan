309c3e86b8f1410466bf5ca097df4afb64ddbab4
celder0 <celder0@me.com>
2014-04-19 13:41:49 -0400

    Coin now appears over column when selected need to figure out how to make it go away when dragged to another column. When column is released, game changes turns. Waiting on game logic and working on animation later.

--- Connect-4/ViewController.m
+++ Connect-4/ViewController.m
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *BluePlayerIndicator;
 @property (weak, nonatomic) IBOutlet UIButton *ChangeTurnButton;
 @property (nonatomic) BOOL redPlayersTurn;
+@property (weak, nonatomic) IBOutlet UILabel *winnerLabel;
 @end
 
 @implementation ViewController
@@ -32,15 +33,54 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)selectColumn:(id)sender {
+    UIButton *columnButton = [self.Columns objectAtIndex:[self.Columns indexOfObject:sender]];
+    
+    CGPoint buttonPosition = columnButton.frame.origin;
+    
+    UIImageView *coin = [[UIImageView alloc] initWithFrame:CGRectMake(buttonPosition.x, buttonPosition.y - 50, 45, 40)];
+    
+    if (self.redPlayersTurn) {
+        coin.image = [UIImage imageNamed:@"red_coin"];
+    }
+    else
+    {
+        coin.image = [UIImage imageNamed:@"blue_coin"];
+    }
+    
+    coin.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:coin];
+}
+
 - (IBAction)releaseCoin:(id)sender  {
+    int currentCoin = 2;
+    if (self.redPlayersTurn)
+    {
+        currentCoin = 1;
+    }
     
-    
-    
+    //if (![self.game isColumnFull])
+    {
+        int columnIndex = [self.Columns indexOfObject:sender];
+      //  [self.game dropCoin:currentCoin inColumn:columnIndex];
+        //if (self.game isWinner:columnIndex coinType:currentCoin) {
+            if (self.redPlayersTurn) {
+                [self.winnerLabel setText:@"Red Wins"];
+            }
+            else
+            {
+                [self.winnerLabel setText:@"Blue Wins"];
+            }
+      //  }
+        
+        
+        [self changeTurns];
+    }
 }
 
 
 
-- (IBAction)changeTurns:(id)sender {
+- (void)changeTurns{
     
     if (self.redPlayersTurn == false) {
         [self.RedPlayerIndicator setImage:[UIImage imageNamed:@"red_coin"]];
